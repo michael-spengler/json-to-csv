477fb405c029d18d8405c9adc272f51fce374f96
Michael Spengler <mycamper@gmx.de>
2020-07-08 19:01:00 +0200

    simplifications

---
+++ server/.env-template.ts
@@ -0,0 +1,4 @@
+export const httpsPort = 0
+export const httpPort = 4443
+export const pathToCert = '/etc/letsencrypt/live/yourfolder/cert.pem'
+export const pathToKey = '/etc/letsencrypt/live/yourfolder/privkey.pem'
